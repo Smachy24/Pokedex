a4a6c7a60853f0a667611164806b3733c0aab9b6
Smachy <pro.diegocdossantos@gmail.com>
2023-03-01 21:51:26 +0100

    init pokemon page

--- getPokemon.js
+++ getPokemon.js
@@ -1,13 +1,21 @@
 
-async function fetchPokemon(){
-  let fetchData = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+async function fetchPokemon(name){
+  let fetchData = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   let pokemonData = await fetchData.json()
   let pokemonSprite = pokemonData.sprites.front_default
-  console.log(pokemonData)
+  let pokemonName = pokemonData.forms[0].name
+  let pokemonDescription = null;
+  let pokemonPokedexNumber = null;
+  let pokemonEvolutions = null;
+  let pokemonStats = null;
+  let pokemonMoves = null;
+  let pokemonShinySprite = null;
+  
+  
+  document.getElementsByClassName("pokemon-name")[0].innerHTML = "Name : " + pokemonName
   
   spriteEl = document.getElementsByClassName("pokemon-sprite");
   spriteEl[0].src = pokemonSprite
-  console.log(spriteEl.src)
 }
 
-// fetchPokemon()
+fetchPokemon("ditto")
