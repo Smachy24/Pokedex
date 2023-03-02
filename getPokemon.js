ca663cb26ef996f395f1d02de7c278976440cd62
Smachy <pro.diegocdossantos@gmail.com>
2023-03-02 19:01:21 +0100

    data pokemon

--- getPokemon.js
+++ getPokemon.js
@@ -1,21 +1,30 @@
 
 async function fetchPokemon(name){
-  let fetchData = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  let pokemonData = await fetchData.json()
-  let pokemonSprite = pokemonData.sprites.front_default
-  let pokemonName = pokemonData.forms[0].name
-  let pokemonDescription = null;
-  let pokemonPokedexNumber = null;
-  let pokemonEvolutions = null;
-  let pokemonStats = null;
-  let pokemonMoves = null;
-  let pokemonShinySprite = null;
+  const fetchData = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const pokemonData = await fetchData.json()
+  const pokemonSprite = pokemonData.sprites.front_default
+  const pokemonShinySprite = pokemonData.sprites.front_shiny;
+  const pokemonName = pokemonData.forms[0].name
+  const pokemonId = pokemonData.id;
+  const pokemonStats = pokemonData.stats[0].stat.name;
+  const pokemonMoves = pokemonData.moves[0].move.name;
+  const data = {"pokemonSprite" : pokemonSprite,"pokemonShinySprite": pokemonShinySprite, "pokemonName": pokemonName, "pokemonId": pokemonId, "pokemonStats": pokemonStats, "pokemonMoves":pokemonMoves};
   
-  
-  document.getElementsByClassName("pokemon-name")[0].innerHTML = "Name : " + pokemonName
-  
-  spriteEl = document.getElementsByClassName("pokemon-sprite");
-  spriteEl[0].src = pokemonSprite
+  return data;
 }
 
-fetchPokemon("ditto")
+function showPokemonInfo(data){
+  document.getElementsByClassName("pokemon-name")[0].innerHTML = "Name : " + data.pokemonName;
+  document.getElementsByClassName("pokemon-id")[0].innerHTML = "Pokedex number : " + data.pokemonId;
+  document.getElementsByClassName("pokemon-stats")[0].innerHTML = "Stats : " + data.pokemonStats
+  document.getElementsByClassName("pokemon-moves")[0].innerHTML = "Moves : " + data.pokemonMoves
+  document.getElementsByClassName("pokemon-sprite")[0].src = data.pokemonSprite;
+  document.getElementsByClassName("pokemon-shiny-sprite")[0].src = data.pokemonShinySprite;
+}
+
+async function main(){
+  const dataPokemon = await fetchPokemon("ditto");
+  showPokemonInfo(dataPokemon);
+}
+
+// main()
